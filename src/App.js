1c13a1d703b6f6269e10bf6910a6c291289b2c6e
jassmin-herr <jherr0905@gmail.com>
2021-06-07 07:06:04 -0500

    wip: able to add items

--- src/App.js
+++ src/App.js
@@ -1,38 +1,79 @@
-import { useState } from "react";
+
+import { useState, useEffect } from "react";
 import "./App.css";
 
 export default function App() {
   const [state, setState] = useState({
-    medicines: [{ medicine: "Tylenol", level: 4 }]
+    skills: [{ skill: "Tylenol", level: 4 }],
+    newSkill: {
+     skill: "",
+     level: "3"
+    }
   });
 
-  function addMedicine() {
-    alert("ADD SKILL CLICKED");
+  useEffect(function() {
+    function getAppData () {
+    fetch('http://localhost:3001/api/skills')
+    .then(response => response.json())
+    .then(data => setState(prevState =>({
+      ...prevState,
+      skills: data
+      
+    })));
+    }
+
+   getAppData()
+
+  }, []);
+
+  function addSkill(e) {
+    e.preventDefault();
+   // alert("ADD SKILL CLICKED");
+   setState({
+     skills: [...state.skills, state.newSkill],
+     newSkill: {
+       skill: "",
+       level: "3"
+     }
+   });
+
+ }
+
+  function handleChange(e) {
+    setState(prevState => ({
+        skills: prevState.skills,
+        //...prevState, //function, method overloading
+        newSkill: {
+          ...prevState.newSkill,
+           [e.target.name]: e.target.value
+
+       }
+    })); 
   }
 
   return (
     <section>
-      <h2>WelliKids Medicine Tracker</h2>
+      <h2>Kids Medicine Tracker</h2>
       <hr />
-      {state.medicines.map((s, i) => (
+      {state.skills.map((s, i) => (
         <article key={i}>
-          <div>{s.medicine}</div> <div>{s.level}</div>
+          <div>{s.skill}</div> <div>{s.level}</div>
         </article>
       ))}
       <hr />
-      <form>
+      <form onSubmit={addSkill}>
         <label>
           <span>Name</span>
-          <input name="medicine" />
         </label>
+          <input name="skill" value={state.newSkill.skill} onChange={handleChange}/>
         <label>
           <span>Quantity:</span>
-          <select name="level">
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
+          <select name="level" value={state.newSkill.level} onChange={handleChange}>
+            <option value="1ml">1ml</option>
+            <option value="2ml">2ml</option>
+            <option value="3ml">3ml</option>
+            <option value="4ml">4ml</option>
+            <option value="5ml">5ml</option>
           </select>
         </label>
         <button>Add Medicine</button>
